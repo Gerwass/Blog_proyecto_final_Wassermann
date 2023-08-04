4142ebad3430e133391f1b777ea7f493edd2cd31
German Wassermann <wassermann.german@gmail.com>
2023-08-04 20:57:43 -0300

    primeros pasos app blog

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Posteo (models.Model):
+    #los atributos de clase(son las columnas de la tabla)
+    titulo = models.CharField(max_length=64)
+    bajada = models.CharField(max_length=140)
+    creador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posteo_creados', null=True)
+    imagen = models.ImageField(upload_to='media')
+    texto = models.CharField()
+
+    def __str__(self):
+        return f"{self.titulo}, {self.creador}"

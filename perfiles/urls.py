231f47a719ec79307ff41ae6c5ff0cd27acb63d3
German Wassermann <wassermann.german@gmail.com>
2023-08-08 19:20:25 -0300

    Creacion superuser

--- perfiles/urls.py
+++ perfiles/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from perfiles.views import  (registro, login_view, CustomLogoutView,MiPerfilUpdateView,
                              agregar_avatar,about)
 
@@ -15,3 +16,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
